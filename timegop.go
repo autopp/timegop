7f794790785d986ee06b2f052c7c74bc63a23eab
autopp <autopp.inc@gmail.com>
2017-12-06 09:35:08 +0900

    add comment to public functions

--- timegop.go
+++ timegop.go
@@ -7,19 +7,21 @@ import (
 var frozen = false
 var freezedTime time.Time
 
+// Freeze fixes current time as given t
 func Freeze(t time.Time) {
 	frozen = true
 	freezedTime = t
 }
 
+// Now returns a fixed time if Freeze is called, otherwise a real time
 func Now() time.Time {
 	if frozen {
 		return freezedTime
-	} else {
-		return time.Now()
 	}
+	return time.Now()
 }
 
+// Return disable time fixing
 func Return() {
 	frozen = false
 }
