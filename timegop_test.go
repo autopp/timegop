971ca3d4892ce4c5c93064c278f1b68a040e6a84
autopp <autopp.inc@gmail.com>
2017-12-11 09:53:12 +0900

    add spec of Until

--- timegop_test.go
+++ timegop_test.go
@@ -83,4 +83,29 @@ var _ = Describe("Timegop", func() {
 			})
 		})
 	})
+
+	Describe("Until()", func() {
+		u := time.Date(2017, time.December, 6, 8, 0, 0, 0, time.Local)
+
+		Context("Without Freeze()", func() {
+			It("Returns duration from the freezed time to argument", func() {
+				Expect(Until(u)).To(BeNumerically("~", time.Until(u), time.Millisecond))
+			})
+		})
+
+		Context("With Freeze()", func() {
+			JustBeforeEach(func() {
+				Freeze(t)
+			})
+
+			AfterEach(func() {
+				Return()
+			})
+
+			It("Returns duration from the freezed time to argument", func() {
+				expected, _ := time.ParseDuration("-1h29m")
+				Expect(Until(u)).To(Equal(expected))
+			})
+		})
+	})
 })
