3ed29d3458344dcbf2b8fe31289b714e15e37d24
autopp <autopp.inc@gmail.com>
2017-12-11 09:49:41 +0900

    add spec of Since

--- timegop_test.go
+++ timegop_test.go
@@ -10,13 +10,13 @@ import (
 )
 
 var _ = Describe("Timegop", func() {
+	t := time.Date(2017, time.December, 6, 9, 29, 0, 0, time.Local)
+
 	BeRealTime := func() types.GomegaMatcher {
 		return BeTemporally("~", time.Now(), time.Millisecond)
 	}
 
 	Describe("Now()", func() {
-		t := time.Date(2017, time.December, 6, 9, 29, 0, 0, time.Local)
-
 		Context("Without Freeze()", func() {
 			It("Returns real time", func() {
 				Expect(Now()).To(BeRealTime())
@@ -55,6 +55,31 @@ var _ = Describe("Timegop", func() {
 
 			It("Returns real time", func() {
 				Expect(Now()).To(BeRealTime())
+			})
+		})
+	})
+
+	Describe("Since()", func() {
+		u := time.Date(2017, time.December, 6, 8, 0, 0, 0, time.Local)
+
+		Context("Without Freeze()", func() {
+			It("Returns duration from argument to the real time", func() {
+				Expect(Since(u)).To(BeNumerically("~", time.Since(u), time.Millisecond))
+			})
+		})
+
+		Context("With Freeze()", func() {
+			JustBeforeEach(func() {
+				Freeze(t)
+			})
+
+			AfterEach(func() {
+				Return()
+			})
+
+			It("Returns duration from argument to the freezed time", func() {
+				expected, _ := time.ParseDuration("+1h29m")
+				Expect(Since(u)).To(Equal(expected))
 			})
 		})
 	})
